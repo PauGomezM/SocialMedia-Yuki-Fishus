43a1446c156d41966e84697067a1e9d067a8766c
PauGM <125443297+PauGomezM@users.noreply.github.com>
2023-07-20 15:09:54 +0200

    Just added the whole project

---
+++ main.js
@@ -0,0 +1,110 @@
+var fbYuki = document.querySelector(".yuki-fb-links");
+var fbFishus = document.querySelector(".fishus-fb-links");
+var igYuki = document.querySelector(".yuki-ig-links"); 
+var igFishus = document.querySelector(".fishus-ig-links");
+var ytYuki = document.querySelector(".yuki-yt-links");
+var ytFishus = document.querySelector(".fishus-yt-links");
+
+var iconsY = [document.querySelector("#icon-fb-yuki"), document.querySelector("#icon-ig-yuki"), document.querySelector("#icon-yt-yuki")];
+var iconsF = [document.querySelector("#icon-fb-fishus"), document.querySelector("#icon-ig-fishus"), document.querySelector("#icon-yt-fishus")];
+
+function showElement(element) {
+    element.style.visibility = "visible";
+    element.style.display = "flex";
+}
+
+function hideElement(element) {
+    element.style.visibility = "hidden";
+    element.style.display = "none";
+}
+
+function fadeOutElement(element) {
+    element.classList.add("fade-out");
+}
+
+function removeFadeOutElement(element) {
+    element.classList.remove("fade-out");
+}
+
+function toggleElements(elementToShow) {
+    if (elementToShow === fbYuki) {
+        showElement(fbYuki);
+        hideElement(igYuki);
+        hideElement(ytYuki);
+        removeFadeOutElement(iconsY[0]);
+        fadeOutElement(iconsY[1]);
+        fadeOutElement(iconsY[2]);
+    } else if (elementToShow === igYuki) {
+        showElement(igYuki);
+        hideElement(fbYuki);
+        hideElement(ytYuki);
+        removeFadeOutElement(iconsY[1]);
+        fadeOutElement(iconsY[0]);
+        fadeOutElement(iconsY[2]);
+    } else if (elementToShow === ytYuki) {
+        showElement(ytYuki);
+        hideElement(fbYuki);
+        hideElement(igYuki);
+        removeFadeOutElement(iconsY[2]);
+        fadeOutElement(iconsY[0]);
+        fadeOutElement(iconsY[1]);
+    } else if (elementToShow === fbFishus) {
+        showElement(fbFishus);
+        hideElement(igFishus);
+        hideElement(ytFishus);
+        removeFadeOutElement(iconsF[0]);
+        fadeOutElement(iconsF[1]);
+        fadeOutElement(iconsF[2]);
+    } else if (elementToShow === igFishus) {
+        showElement(igFishus);
+        hideElement(fbFishus);
+        hideElement(ytFishus);
+        removeFadeOutElement(iconsF[1]);
+        fadeOutElement(iconsF[0]);
+        fadeOutElement(iconsF[2]);
+    } else {
+        showElement(ytFishus);
+        hideElement(fbFishus);
+        hideElement(igFishus);
+        removeFadeOutElement(iconsF[2]);
+        fadeOutElement(iconsF[0]);
+        fadeOutElement(iconsF[1]);
+    }
+}
+
+const yukiSection = document.querySelector(".yuki-section");
+const fishusSection = document.querySelector(".fishus-section");
+const yuki = document.querySelector(".yuki");
+const fishus = document.querySelector(".fishus");
+const yukiBubble = document.querySelector(".Yuki-bubble");
+const fishusBubble = document.querySelector(".Fishus-bubble");
+
+function resetStyles() {
+    yukiSection.style.display = "none";
+    yuki.style.display = "flex";
+    yuki.style.height = "0";
+    yukiBubble.style.display = "none";
+  
+    fishusSection.style.display = "none";
+    fishus.style.display = "flex";
+    fishus.style.height = "0";
+    fishusBubble.style.display = "none";
+  }
+  
+  function showYuki() {
+    resetStyles();
+    yukiSection.style.display = "flex";
+    yuki.style.height = "100vh";
+    fishusBubble.style.display = "flex";
+    fishus.style.display = "none";
+    console.log("Yuki bubble clicked");
+  }
+  
+  function showFishus() {
+    resetStyles();
+    fishusSection.style.display = "flex";
+    fishus.style.height = "100vh";
+    yukiBubble.style.display = "flex";
+    yuki.style.display = "none";
+    console.log("Fishus bubble clicked");
+  }
